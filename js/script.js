0e74ee8de755bdf8717c94ca7a9aafa9ebe03958
GreyM22 <greimuka@gmail.com>
2019-11-11 14:51:39 +0100

    finishing the toggle nav bar

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("menu-button").addEventListener("click", function(){
+    $(".bottom-bar").addClass("clicked");
+});
+
+// $(document).ready(function(){
+//     $("#menu-button").click(function(){
+//         $(".bottom-bar").addClass("clicked");
+//     })
+//   });
