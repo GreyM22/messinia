db25b6e6305e201ceed9b98df84285d0ef35dfde
GreyM22 <greimuka@gmail.com>
2020-02-05 17:24:42 +0100

    testing the form

--- js/script.js
+++ js/script.js
@@ -177,91 +177,91 @@ $(document).ready(function () {
 
     // Fetch the form element
 
-    function getFormDataString(formEl) {
-        var formData = new FormData(formEl),
-            data = [];
+    // function getFormDataString(formEl) {
+    //     var formData = new FormData(formEl),
+    //         data = [];
 
-        for (var keyValue of formData) {
-            data.push(
-                encodeURIComponent(keyValue[0]) + "=" + encodeURIComponent(keyValue[1])
-            );
-        }
+    //     for (var keyValue of formData) {
+    //         data.push(
+    //             encodeURIComponent(keyValue[0]) + "=" + encodeURIComponent(keyValue[1])
+    //         );
+    //     }
 
-        return data.join("&");
-    }
+    //     return data.join("&");
+    // }
 
-    var formEmail = document.getElementById("reserve-form");
+    // var formEmail = document.getElementById("reserve-form");
 
-    // Override the submit event
-    formEmail.addEventListener("submit", function (e) {
-        e.preventDefault();
+    // // Override the submit event
+    // formEmail.addEventListener("submit", function (e) {
+    //     e.preventDefault();
 
-        let request = new XMLHttpRequest();
+    //     let request = new XMLHttpRequest();
 
-        request.addEventListener("load", function () {
-            if (request.status === 302) {
-                // CloudCannon redirects on success
-            }
-        });
+    //     request.addEventListener("load", function () {
+    //         if (request.status === 302) {
+    //             // CloudCannon redirects on success
+    //         }
+    //     });
 
-        // Validation of the reservation form
+    //     // Validation of the reservation form
 
-        let date = $("#date").val();
-        let month = date.split("/")[0];
-        let day = date.split("/")[1];
-        let time = $("#time").val();
+    //     let date = $("#date").val();
+    //     let month = date.split("/")[0];
+    //     let day = date.split("/")[1];
+    //     let time = $("#time").val();
 
-        let currentDate = new Date();
+    //     let currentDate = new Date();
 
-        if (parseInt(month) > parseInt(currentDate.getMonth()) + 1) {
-            request.open(formEmail.method, formEmail.action);
-            request.setRequestHeader(
-                "Content-Type",
-                "application/x-www-form-urlencoded"
-            );
-            request.send(getFormDataString(formEmail));
-            $("footer .sms-form").text("Reservation made successfully");
-            $(" footer form").trigger("reset");
-            $("footer").animate({ scrollTop: 0 }, "slow");
-        } else if (
-            parseInt(month) == parseInt(currentDate.getMonth() + 1) &&
-            parseInt(day) >= parseInt(currentDate.getDate())
-        ) {
-            if (parseInt(day) > parseInt(currentDate.getDate())) {
-                request.open(formEmail.method, formEmail.action);
-                request.setRequestHeader(
-                    "Content-Type",
-                    "application/x-www-form-urlencoded"
-                );
-                request.send(getFormDataString(formEmail));
-                $("footer form ").trigger("reset");
-                $("footer").animate({ scrollTop: 0 }, "slow");
-                $("footer .sms-form").text("Reservation made successfully");
-            } else if (
-                parseInt(day) == parseInt(currentDate.getDate()) &&
-                parseInt(time.split(":")[0]) >= parseInt(currentDate.getHours()) + 2 &&
-                parseInt(time.split(":")[1]) >= parseInt(currentDate.getMinutes())
-            ) {
-                request.open(formEmail.method, formEmail.action);
-                request.setRequestHeader(
-                    "Content-Type",
-                    "application/x-www-form-urlencoded"
-                );
-                request.send(getFormDataString(formEmail));
-                $("footer form ").trigger("reset");
-                $("footer").animate({ scrollTop: 0 }, "slow");
-                $("footer .sms-form").text("Reservation made successfully");
-            } else {
-                $("footer .sms-form").text("*Pleas book two hours in advance");
-                $("footer").animate({ scrollTop: 0 }, "slow");
-            }
-        } else {
-            $("footer .sms-form").text(
-                "*You can not reserve a table in the past. Please change your reservation date."
-            );
-            $("footer").animate({ scrollTop: 0 }, "slow");
-        }
-    });
+    //     if (parseInt(month) > parseInt(currentDate.getMonth()) + 1) {
+    //         request.open(formEmail.method, formEmail.action);
+    //         request.setRequestHeader(
+    //             "Content-Type",
+    //             "application/x-www-form-urlencoded"
+    //         );
+    //         request.send(getFormDataString(formEmail));
+    //         $("footer .sms-form").text("Reservation made successfully");
+    //         $(" footer form").trigger("reset");
+    //         $("footer").animate({ scrollTop: 0 }, "slow");
+    //     } else if (
+    //         parseInt(month) == parseInt(currentDate.getMonth() + 1) &&
+    //         parseInt(day) >= parseInt(currentDate.getDate())
+    //     ) {
+    //         if (parseInt(day) > parseInt(currentDate.getDate())) {
+    //             request.open(formEmail.method, formEmail.action);
+    //             request.setRequestHeader(
+    //                 "Content-Type",
+    //                 "application/x-www-form-urlencoded"
+    //             );
+    //             request.send(getFormDataString(formEmail));
+    //             $("footer form ").trigger("reset");
+    //             $("footer").animate({ scrollTop: 0 }, "slow");
+    //             $("footer .sms-form").text("Reservation made successfully");
+    //         } else if (
+    //             parseInt(day) == parseInt(currentDate.getDate()) &&
+    //             parseInt(time.split(":")[0]) >= parseInt(currentDate.getHours()) + 2 &&
+    //             parseInt(time.split(":")[1]) >= parseInt(currentDate.getMinutes())
+    //         ) {
+    //             request.open(formEmail.method, formEmail.action);
+    //             request.setRequestHeader(
+    //                 "Content-Type",
+    //                 "application/x-www-form-urlencoded"
+    //             );
+    //             request.send(getFormDataString(formEmail));
+    //             $("footer form ").trigger("reset");
+    //             $("footer").animate({ scrollTop: 0 }, "slow");
+    //             $("footer .sms-form").text("Reservation made successfully");
+    //         } else {
+    //             $("footer .sms-form").text("*Pleas book two hours in advance");
+    //             $("footer").animate({ scrollTop: 0 }, "slow");
+    //         }
+    //     } else {
+    //         $("footer .sms-form").text(
+    //             "*You can not reserve a table in the past. Please change your reservation date."
+    //         );
+    //         $("footer").animate({ scrollTop: 0 }, "slow");
+    //     }
+    // });
     /*********************************************************/
 
     // slide in animation
