2b8d7297fed0639e3106e5f352490268702ccdcc
GreyM22 <greimuka@gmail.com>
2020-02-07 14:22:30 +0100

    made the gallery

--- js/script.js
+++ js/script.js
@@ -317,54 +317,6 @@ $(document).ready(function () {
     });
 });
 
-/* Fromating the date in the booking form  */
-// var date = document.getElementById("date");
-
-// function checkValue(str, max) {
-//     if (str.charAt(0) !== "0" || str == "00") {
-//         var num = parseInt(str);
-//         if (isNaN(num) || num <= 0 || num > max) num = 1;
-//         str =
-//             num > parseInt(max.toString().charAt(0)) && num.toString().length == 1
-//                 ? "0" + num
-//                 : num.toString();
-//     }
-//     return str;
-// }
-
-// date.addEventListener("input", function (e) {
-//     this.type = "text";
-//     var input = this.value;
-//     if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
-//     var values = input.split("/").map(function (v) {
-//         return v.replace(/\D/g, "");
-//     });
-//     if (values[0]) values[0] = checkValue(values[0], 12);
-//     if (values[1]) values[1] = checkValue(values[1], 31);
-//     var output = values.map(function (v, i) {
-//         return v.length == 2 && i < 1 ? v + " / " : v;
-//     });
-//     this.value = output.join("").substr(0, 14);
-// });
-
-/* to format the time input */
-// var time = document.getElementById("time");
-
-// time.addEventListener("input", function (e) {
-//     this.type = "text";
-//     var input = this.value;
-//     if (/\D\:$/.test(input)) input = input.substr(0, input.length - 3);
-//     var values = input.split(":").map(function (v) {
-//         return v.replace(/\D/g, "");
-//     });
-//     if (values[0]) values[0] = checkValue(values[0], 23);
-//     if (values[1]) values[1] = checkValue(values[1], 60);
-//     var output = values.map(function (v, i) {
-//         return v.length == 2 && i < 1 ? v + " : " : v;
-//     });
-//     this.value = output.join("").substr(0, 14);
-// });
-
 (function ($) {
     "use strict";
     $(".animsition").animsition({
@@ -372,7 +324,7 @@ $(document).ready(function () {
         outClass: "fade-out",
         inDuration: 500,
         outDuration: 500,
-        linkElement: 'a:not([target="_blank"]):not([href^="#"])',
+        linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([href$=".jpg"]):not([href$=".png"])',
         loading: true,
         loadingParentElement: "body",
         loadingClass: "animsition-loading2",
